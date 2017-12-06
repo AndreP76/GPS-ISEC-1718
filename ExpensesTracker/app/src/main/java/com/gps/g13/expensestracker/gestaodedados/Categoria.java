f8a4aad3db18e1adfa5f1e583f4f12a65a984399
LidiaSousaf <lidia_sousaf@hotmail.com>
2017-12-06 16:46:10 +0000

    Categoria iniciada

--- ExpensesTracker/app/src/main/java/com/gps/g13/expensestracker/gestaodedados/Categoria.java
+++ ExpensesTracker/app/src/main/java/com/gps/g13/expensestracker/gestaodedados/Categoria.java
@@ -11,7 +11,29 @@ public abstract class Categoria implements Serializable {
         return nome;
     }
 
-    public void adicionarTransacao(Transacao transacao){
+    public void adicionarTransacao(Transacao transacao) {
+        if (transacao != null) {
+            transacoes.add(transacao);
+        }
+    }
 
+    public void removeTransacao(Transacao transacao) {
+        if (transacao != null) {
+            transacoes.remove(transacao);
+        }
+    }
+
+    public abstract double getResumoDeTransacoes();
+
+    public List<Transacao> getListaDeTransacoes() {
+        return transacoes;
+    }
+
+    public Transacao getTransacao(int index) {
+        if (index < 0 || index >= transacoes.size()) {
+            return null;
+        } else {
+            return transacoes.get(index);
+        }
     }
 }
