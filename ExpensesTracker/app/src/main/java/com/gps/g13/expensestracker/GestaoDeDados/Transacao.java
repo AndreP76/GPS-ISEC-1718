b6d881d94b54e02dd04692fc94299d2c68d29567
Chamuscado <32516659+Chamuscado@users.noreply.github.com>
2017-12-06 16:16:34 +0000

    Transacao adicionada
    Transacao adicionada

--- ExpensesTracker/app/src/main/java/com/gps/g13/expensestracker/GestaoDeDados/Transacao.java
+++ ExpensesTracker/app/src/main/java/com/gps/g13/expensestracker/GestaoDeDados/Transacao.java
@@ -1,11 +1,40 @@
 package com.gps.g13.expensestracker.GestaoDeDados;
 
 import java.io.Serializable;
+import java.util.Date;
 
-/**
- * Created by Chamuscado on 06/12/2017.
- */
+public class Transacao implements Serializable {
+    private Double montante;
+    private Date data;
+    private String nome;
 
-public class Transacao implements Serializable{
+    public Transacao(Double montante, Date data, String nome) {
+        this.montante = montante;
+        this.data = data;
+        this.nome = nome;
+    }
 
+    public Double getMontante() {
+        return montante;
+    }
+
+    public void setMontante(Double montante) {
+        this.montante = montante;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
