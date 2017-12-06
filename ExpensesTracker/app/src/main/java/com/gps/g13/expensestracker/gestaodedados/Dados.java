e7e4c44f87a7dc37aafb382d8dfbb5589a2564c3
Chamuscado <32516659+Chamuscado@users.noreply.github.com>
2017-12-06 17:11:43 +0000

    enumeração ListaNomesCategoriasDespesas adicionada

--- ExpensesTracker/app/src/main/java/com/gps/g13/expensestracker/gestaodedados/Dados.java
+++ ExpensesTracker/app/src/main/java/com/gps/g13/expensestracker/gestaodedados/Dados.java
@@ -1,12 +1,44 @@
 package com.gps.g13.expensestracker.gestaodedados;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class Dados implements Serializable {
-    private List<Categoria> categorias;
+    private Map<String, Categoria> categorias;
+
+    public Dados() {
+        categorias = new HashMap<>();
+        ListaNomesCategoriasDespesas[] nomes = ListaNomesCategoriasDespesas.values();
+        for (ListaNomesCategoriasDespesas i : nomes) {
+            categorias.put(i.name(), new CategoriaDespesas(i.name()));
+        }
+        categorias.put("Rendimentos", new CategoriaRendimento());
+    }
+
+    public Map getCategorias() {
+        return categorias;
+    }
+
+    public List getTransacoesCategoria(String nome) {
+        Categoria cat = categorias.get(nome);
+        return cat != null ? cat.getListaDeTransacoes() : null;
+    }
+
+    public boolean adicionaTransacao(String categoria, String nome, double montante, Date data) {
+        return;
+    }
+
+    public boolean removeTransacao(String categoria, String nome) {
+        return true;
+    }
+
+    public boolean editaTransacao(String categoria, String nome, double montante, Date data) {
+        return true;
+    }
 
 
 }
-
---
+++ ExpensesTracker/app/src/main/java/com/gps/g13/expensestracker/gestaodedados/ListaNomesCategoriasDespesas.java
@@ -0,0 +1,15 @@
+package com.gps.g13.expensestracker.gestaodedados;
+
+/**
+ * Created by Chamuscado on 06/12/2017.
+ */
+
+public enum ListaNomesCategoriasDespesas {
+    Alimentação,
+    Alojamento,
+    Transportes,
+    Universidade,
+    Lazer,
+    Outros
+
+}
